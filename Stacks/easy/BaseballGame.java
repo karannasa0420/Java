cb571ff517c9c6df669f9a228cda771323e31023
karan Nasa <karannasa0000@hotmail.com>
2024-05-04 17:01:53 +0530

    stack easy

---
+++ Stacks/easy/BaseballGame.java
@@ -0,0 +1,43 @@
+package Stacks.easy;
+
+import java.util.Stack;
+
+public class BaseballGame {
+
+  public int calPoints(String[] operations) {
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0; i < operations.length; i++) {
+      if (operations[i].equals("C")) {
+        if (!stack.isEmpty()) {
+          stack.remove(stack.size() - 1);
+        }
+      } else if (operations[i].equals("D")) {
+        int lastElement = 2 * stack.get(stack.size() - 1);
+        stack.add(lastElement);
+      } else if (operations[i].equals("+")) {
+        int sumElement =
+          stack.get(stack.size() - 2) + stack.get(stack.size() - 1);
+        stack.add(sumElement);
+      } else {
+        int strtoInt = Integer.parseInt(operations[i]);
+        stack.add(strtoInt);
+      }
+    }
+    int finalAnswer = 0;
+    for (int i = stack.size() - 1; i >= 0; i--) {
+      finalAnswer = finalAnswer + stack.get(i);
+    }
+    return finalAnswer;
+  }
+
+  public static void main(String[] args) {
+    BaseballGame BaseballGame = new BaseballGame();
+
+    // Test cases
+    String[] test1 = { "5", "2", "C", "D", "+" }; // Expected output: 30
+    String[] test2 = { "5", "-2", "4", "C", "D", "9", "+", "+" }; // Expected output: 27
+
+    System.out.println("Test 1 result: " + BaseballGame.calPoints(test1));
+    System.out.println("Test 2 result: " + BaseballGame.calPoints(test2));
+  }
+}
